bbf01ace5640e059b82d3edf2344c66ee5a9ab92
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-09-07 15:14:54 -0700

    Update Creations.js

--- src/Creations.js
+++ src/Creations.js
@@ -1,17 +1,48 @@
 import Header from './Header';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Creations = () => {
+  const [characters, setCharacters] = useState([]);
 
-return (
+  const fetchAllCharacters = async () => {
+    try {
+      const response = await axios.get('https://lorecraft.onrender.com/character');
+      if (response.status === 200) {
+        setCharacters(response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching characters:', error);
+      // Here you might set an error state variable to show an error message to the user
+    }
+  };
 
-      <div className="main">
-<Header />
-See Your Creation
+  useEffect(() => {
+    fetchAllCharacters();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      {characters.length === 0 ? (
+        <p>No characters found</p>
+      ) : (
+        characters.map((character, index) => (
+          <div key={index}>
+         
+            <p>Char Name: {character.charName}</p>
+            <p>Class Type: {character.classType}</p>
+            <p>Alignment: {character.alignment}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Image URL: {character.imageURL}</p>
+            <p>Backstory: {character.backstory}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
 
-
-
 export default Creations;
+
 
