f44ae64a3c2937e6c1a1121c1859062363e68405
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-09-07 19:48:34 -0700

    add new login image and text

--- src/LoggedinComponent.js
+++ src/LoggedinComponent.js
@@ -26,13 +26,22 @@ return (
 <Header />
 <div className="container">
          <div className="row">
-      <div className="col-md-5">
+      <div className="col-md-10">
             <div style={{ border: '1px solid #000', background:'#fff', padding: '10px', boxShadow: '0px 0px 10px #000', maxWidth: '100%', margin: '0 auto' }}>
        {isAuthenticated ? (  // Conditionally render the navigation links based on isAuthenticated value
             <>
 <img src='./images/lorecraft.png' alt='logo' className='loggedinimage'/>      </>
  ) : (
-        <div>  Test</div>
+  <div className="row"> 
+                <div className="col-md-5">
+                  <img src='./images/lorecraftstart.png' alt='logo' className='loggedinimage'/> 
+                </div>                
+                <div className="col-md-7">
+                  <p>
+                    Step into your valorous warrior persona with LoreCraft, where AI crafts epic tales and images based on your name, gender, and fierce personality. Sharpen your blade and ready your shield for a unique adventure that awaits you in a realm created just for you. Unleash the warrior within, as every battle cry and heroic deed is woven into a narrative that mirrors your might and mettle.
+                  </p>
+                </div>
+              </div>
           )}
     </div>  </div>  </div>  </div> </div>
 
