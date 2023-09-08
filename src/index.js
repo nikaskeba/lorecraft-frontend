97574741afd77055dc1de030b555e0f93577ca41
Quasark <q@quasark.xyz>
2023-09-07 23:06:12 -0700

    more changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,5 +14,3 @@ root.render(
     <App />
   </Auth0Provider>
 );
-
-reportWebVitals();
