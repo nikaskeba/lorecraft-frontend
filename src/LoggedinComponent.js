9b063eb9f54b9ef94208100b1e25a8bc1e0beb77
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-09-07 19:17:04 -0700

    Update LoggedinComponent.js

--- src/LoggedinComponent.js
+++ src/LoggedinComponent.js
@@ -1,5 +1,7 @@
 import Header from './Header';
 import About from './About';
+import { useAuth0 } from '@auth0/auth0-react'; // Import the useAuth0 hook
+
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
@@ -10,6 +12,7 @@ const pattern_with_prefix = /(.*?)\(([^()]+)\)([^()]+)\(([^()]+)\)/;
 
 const LoggedInComponent = () => {
   
+    const { isAuthenticated } = useAuth0(); // Call the useAuth0 hook to get the isAuthenticated variable
 
 
 
@@ -25,8 +28,12 @@ return (
          <div className="row">
       <div className="col-md-5">
             <div style={{ border: '1px solid #000', background:'#fff', padding: '10px', boxShadow: '0px 0px 10px #000', maxWidth: '100%', margin: '0 auto' }}>
-
-<img src='./images/lorecraft.png' alt='logo' className='loggedinimage'/>
+       {isAuthenticated ? (  // Conditionally render the navigation links based on isAuthenticated value
+            <>
+<img src='./images/lorecraft.png' alt='logo' className='loggedinimage'/>      </>
+ ) : (
+        <div>  Test</div>
+          )}
     </div>  </div>  </div>  </div> </div>
 
   );
